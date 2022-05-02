c4fba719e5a5578a5d290c7bc19da5af55009f9c
Mayank Patel <mayank509.mp@gmail.com>
2022-05-02 23:32:21 +0530

    Revert "added database API"
    This reverts commit a60ed3d913c6df88752913909803fa491597056b.

--- API/config.js
+++
@@ -1,5 +0,0 @@
-let config = {
-    dbUrl: "mongodb+srv://mayank_test_demo:mayank_demo123@cluster0.qho8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-};
-
-module.exports = config;
--- API/server.js
+++ API/server.js
@@ -1,38 +1,25 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require("express")
+const mongo = require("mongodb").MongoClient
+const app = express()
+/*add get post methods here*/
+app.listen(3000, () => console.log("Server ready"))
+const url = "mongodb://localhost:27017"
 
-const port = 3031;
-const config = require("./config");
+app.use(express.json())
+let db
 
-const usersRouter = require("./routes/users");
-
-app.use(logger("dev"));
-
-const dbUrl = config.dbUrl;
-
-var options = {
-  keepAlive: true,
-  connectTimeoutMS: 30000,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-mongoose.connect(dbUrl, options, (err) => {
-  if (err) console.log(err);
-  else console.log("Connected to Database");
-});
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use("/users", usersRouter);
-
-app.listen(port, function () {
-  console.log("Runnning on " + port);
-});
-
-module.exports = app;
+mongo.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+	console.log("Connected!")
+    //db = client.db("DBName")
+  }
+)
