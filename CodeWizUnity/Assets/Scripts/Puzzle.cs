d2a3bd2f5d0853d7d6f95b6194f8792dae4e1c5f
Anonymous1609 <97177677+Mystic-Memoirs@users.noreply.github.com>
2022-08-13 19:25:28 +0530

    Fixed loop puzzle

--- CodeWizUnity/Assets/Scripts/Puzzle.cs
+++ CodeWizUnity/Assets/Scripts/Puzzle.cs
@@ -1,18 +1,20 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Puzzle : MonoBehaviour
 {
+    int levelNumber = 0;
     //no of rings, from shlok's formula
-    int n = 3;
+    int n;
 
     [SerializeField]
     GameObject[] rings;
 
     //1 unit rotation = 30 degree clockwise
     //rotation speed, degrees per second
-    private float rotationSpeed = 15.0f;
+    private float rotationSpeed = 30.0f;
     private float animSpeed = 0.01f;
 
     //random generation
@@ -24,58 +26,50 @@ public class Puzzle : MonoBehaviour
     //user guess
     private float guess;
 
-    private int i;
+    private int ringNo;
 
-    //capture numeric input for testing
-    private KeyCode[] keyCodes = {
-         KeyCode.Alpha1,
-         KeyCode.Alpha2,
-         KeyCode.Alpha3,
-         KeyCode.Alpha4,
-         KeyCode.Alpha5,
-         KeyCode.Alpha6,
-         KeyCode.Alpha7,
-         KeyCode.Alpha8,
-         KeyCode.Alpha9,
-     };
 
     //flag
-    private bool done = true;
+    private bool done = false;
     private bool completed = false;
 
 
     void Start()
     {
-
-        Debug.Log(Login.currentStudent.fullname);
-        i = n - 1;
-
+        startGame();
+    }
+    void startGame()
+    {
+        //n = DynamicDifficulty.getinitialN(levelNumber)+1;
+        n = 3;
+        //Debug.Log(Login.currentStudent.fullname);
+        ringNo = n - 1;
+        
         //disabling all rings
         disable();
 
         //random generation of answer
-        correctRotation = Random.Range(1, 10);
+        //correctRotation = Random.Range(1, 3);
+        correctRotation = 2;
         timeLeft = Mathf.Round(correctRotation);
-        Debug.Log(timeLeft);
-
+        Debug.Log(correctRotation);
+        //Debug.Log(timeLeft);
+        
         //enabling only the required rings, as per value of n
         for (int i = 0; i < n; i++)
         {
             rings[i].SetActive(true);
+            rings[i].transform.Rotate(Vector3.back,rotationSpeed*Mathf.Pow(correctRotation,i+1));
         }
+        done = true;
     }
-
 
     void Update()
     {
-        //countdown
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            done = false;
-        }
-
+/*
         if (done == false && completed == false)
         {
+            //Debug.Log(n);
             timeLeft -= 0.01f;
             if (timeLeft > 0)
             {
@@ -83,14 +77,14 @@ public class Puzzle : MonoBehaviour
                 {
                     //nth ring will rotate at speed of n x rotationSpeed 
                     rings[i].transform.Rotate(Vector3.back, (Mathf.Pow(rotationSpeed, (i + 1))) * 0.01f);
-                    Debug.Log(rotationSpeed * (i + 1));
+                    *//*Debug.Log(rotationSpeed * (i + 1));*//*
                 }
             }
             else
             {
                 done = true;
             }
-        }
+        }*/
 
         if (done == true)
         {
@@ -99,39 +93,54 @@ public class Puzzle : MonoBehaviour
                 guess -= animSpeed;
                 if (guess > 0)
                 {
-                    if (i >= 0)
-                    {
-                        rings[i].transform.Rotate(Vector3.forward, (Mathf.Pow(rotationSpeed, (i + 1))) * animSpeed);
-                    }
+
+                    rings[ringNo].transform.Rotate(Vector3.forward, (Mathf.Pow(rotationSpeed, 1)) * animSpeed);
+
                 }
-                else
-                {
-                    i--;
-                    guess = guessedRotation;
-                }
-            }
-            else
-            {
-                guessAnswer();
+
             }
         }
     }
-
-    private void guessAnswer()
+    bool isRotating = false;
+    public TMP_InputField answerInputField;
+    public void guessAnswer()
     {
-        for (int i = 0; i < keyCodes.Length; i++)
+        Debug.Log("Guessed");
+        int numberPressed = int.Parse(answerInputField.text);
+       /* guessedRotation = guess = Mathf.Round(numberPressed);
+        Debug.Log(guess);*/
+        completed = true;
+        StartCoroutine(rotate3(numberPressed));
+
+    }
+    IEnumerator rotate3(int x)
+    {
+        //animSpeed = 1f;
+        for(int i=0;i<x;i++)
         {
-            if (Input.GetKeyDown(keyCodes[i]))
+            guess = 1;
+            ringNo = 0;
+            yield return new WaitForSeconds(1);
+            for (int j=0;j<x;j++)
             {
-                int numberPressed = (i + 1);
-                guessedRotation = guess = Mathf.Round(numberPressed);
-                Debug.Log(guess);
-                completed = true;
-                break;
+                guess = 1;
+                ringNo = 1;
+                yield return new WaitForSeconds(1);
+                for (int k=0;k<x;k++)
+                {
+                    guess = 1;
+                    ringNo = 2;
+                    yield return new WaitForSeconds(1);
+                }
+                
             }
         }
     }
-
+    /*void rotate(int guess,int i,int animSpeed)
+    {
+        
+        
+    }*/
     void disable()
     {
         //disable all rings
@@ -140,5 +149,5 @@ public class Puzzle : MonoBehaviour
             rings[i].SetActive(false);
         }
     }
+
 }
-*/
